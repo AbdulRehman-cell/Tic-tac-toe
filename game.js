7e5bd42b9a63b79afa375b94e69b822c0daba37e
AbdulRehman-cell <abdulrehmanishaque32@gmail.com>
2026-06-27 23:24:46 +0100

    ForgeAI: Auto-fix 5 file(s)

--- game.js
+++ game.js
@@ -1,29 +1,8 @@
 let boxes = document.querySelectorAll(".but");
-let rreset = querySelector("#reset")
-
-rreset.addEventListener("click", () => {
-
-    for (box of boxes) {
-
-        box.innerText = "";
-        box.disabled = false;
-        i = 0;
-        statu = false;
-
-
-    }
-
-    document.querySelector("h2").innerText = "";
-
-
-
-
-
-
-});
-
-
+let rreset = document.querySelector("#reset");
 let turn0 = true;
+let i = 0;
+let statu = false;
 
 const winningPatterns = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -33,63 +12,59 @@ const winningPatterns = [
     [0, 4, 8],
     [2, 4, 6]
 ];
-let i = 0;
-let statu = false;
+
+rreset.addEventListener("click", () => {
+    for (let box of boxes) {
+        box.innerText = "";
+        box.disabled = false;
+    }
+    i = 0;
+    statu = false;
+    turn0 = true;
+    document.querySelector("h2").innerText = "";
+    document.querySelector("h2").style.visibility = "hidden";
+});
+
 boxes.forEach((but) => {
     but.addEventListener("click", () => {
-        if (turn0 === true) {
+        if (turn0) {
             but.innerText = "O";
             turn0 = false;
-            i++;
         } else {
             but.innerText = "X";
             turn0 = true;
-            i++;
         }
+        i++;
         but.disabled = true;
         Checkwinner();
         if (i === 9 && statu === false) {
             document.querySelector("h2").innerText = "DRAW !";
-            document.querySelector("h2").style.display = "visible";
+            document.querySelector("h2").style.visibility = "visible";
         }
-
     });
 });
 
-
-
-const Checkwinner = () => {
+function Checkwinner() {
     for (let pattern of winningPatterns) {
         let pos1 = boxes[pattern[0]].innerText;
         let pos2 = boxes[pattern[1]].innerText;
         let pos3 = boxes[pattern[2]].innerText;
-
 
         if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
             if (pos1 === pos2 && pos2 === pos3) {
                 statu = true;
                 let winner = pos1;
                 announce(winner);
-                break;
-
+                return;
             }
-
-
         }
     }
+}
 
-};
-
-let announce = (winner) => {
-
+function announce(winner) {
     document.querySelector("h2").innerText = "WINNER : " + winner;
     document.querySelector("h2").style.visibility = "visible";
     for (let box of boxes) {
-
         box.disabled = true;
-
     }
-
-
-
 }
